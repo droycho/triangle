757ca94a549e0ddfbdfc4ef06673f595533acebc
DJ Roychoudhury and Joshua Logan <joshua@>
2016-03-24 16:53:12 -0700

    tried to add another logic function, our math seems to be off...

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,24 @@ $(document).ready(function() {
     var side2= $("#side2").val();
     var side3= $("#side3").val();
     console.log(side1, side2, side3);
-    if (side1 === side2 && side2 === side3 && side1 != 0 && side2 != 0 && side3 !=0) {
+
+    // if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
+      alert("Not a triangle! Try again.")
+    } else if (side1 === side2 && side2 === side3 && side1 != 0 && side2 != 0 && side3 !=0) {
       $(".equilateral").show();
       $(".isosceles").hide();
       $(".scalene").hide();
-      // $(this).remove();
     } else if (side1 === side2 && side2 != side3 || side2 === side3 && side3 != side1 || side1 === side3 && side3 != side2) {
       $(".isosceles").show();
       $(".scalene").hide();
       $(".equilateral").hide();
-      // $(this).remove();
     } else if (side1 != side2 && side2 != side3 && side3 != side1) {
       $(".scalene").show();
       $(".isosceles").hide();
       $(".equilateral").hide();
-      // $(this).remove();
     } else {
-      alert("Enter a value for each side!")
+      alert("Please enter a valid number");
     }
     event.preventDefault();
   });
-  // $(".results1").submit(function () {
-  //   $(this).remove();
-  // })
 });
